48b0671629cdc89aad802aa42b611a3f19de0eba
MLVelikova <magibox@yahoo.com>
2023-07-02 16:57:39 +0300

    create new demo project

---
+++ src/main/java/bg/softuni/pathfinder/config/ApplicationBeanConfiguration.java
@@ -0,0 +1,45 @@
+package bg.softuni.pathfinder.config;
+
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+
+@Configuration
+public class ApplicationBeanConfiguration {
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        Converter<String, LocalDateTime> localDateConverter = mappingContext ->
+                LocalDateTime.parse(mappingContext.getSource(), ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
+        Converter<LocalDateTime, String> fromLocalDateConverter = mappingContext ->
+                mappingContext.getSource().toString();
+        modelMapper.addConverter(localDateConverter);
+
+//        modelMapper.typeMap(CustomerSeedDto.class, Customer.class)
+//                .addMappings(mapper -> mapper.using(localDateConverter).map(CustomerSeedDto::getBirthDate, Customer::setBirthDate));
+//
+//        modelMapper.typeMap(Customer.class, CustomerDto.class)
+//                .addMappings(mapper -> mapper.using(fromLocalDateConverter).map(Customer::getBirthDate, CustomerDto::setBirthDate));
+//
+//        modelMapper.typeMap(Sale.class, SaleDto.class)
+//                .addMappings(mapper -> {
+//                    mapper.map(scr -> scr.getCustomer().getName(), SaleDto::setName);
+//
+
+//                    mapper.map(scr->scr.getCar().getParts().stream().map(Part::getPrice)
+//                                   .reduce(BigDecimal.ZERO, BigDecimal::add),SaleDto::setPrice);
+
+         //       });
+
+        return modelMapper;
+    }
+}
---
+++ src/main/java/bg/softuni/pathfinder/util/ValidationUtil.java
@@ -0,0 +1,5 @@
+package bg.softuni.pathfinder.util;
+public interface ValidationUtil {
+
+    <E> boolean isValid(E entity);
+}
